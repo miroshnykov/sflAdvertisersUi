270a6b473e6e49075a9ae11632624cc55b0505ea
miroshnykov <dmytro.miroshnykov@adsurge.com>
2020-04-21 10:18:50 -0400

    debug webpack 2

--- conf/config.js
+++ conf/config.js
@@ -9,8 +9,8 @@ let config
 config = {
     env: process.env.NODE_ENV || env || `production`,
     port: 8080,
-    sflApiAdvertiser:{
-        host:''
+    sflApiAdvertiser: {
+        host: ''
     },
 }
 
