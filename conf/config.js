d3ab6aa108e9896538bd741a66bc757abba1fdfd
miroshnykov <dmytro.miroshnykov@adsurge.com>
2020-04-21 11:08:34 -0400

    add sflApiAdvertiser to config

--- conf/config.js
+++ conf/config.js
@@ -10,7 +10,7 @@ config = {
     env: process.env.NODE_ENV || env || `production`,
     port: 8080,
     sflApiAdvertiser: {
-        host: ''
+        host: '"https://sfl-api-advertiser.surge.systems/"'
     },
 }
 
