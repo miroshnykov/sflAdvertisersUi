684d02531c49df77dc962984f5eb0968a66a2123
miroshnykov <dmytro.miroshnykov@adsurge.com>
2020-04-29 15:06:55 -0400

    AL-116

--- src/api/campaigns.js
+++ src/api/campaigns.js
@@ -19,6 +19,8 @@ const campaigns = async () => {
                         user
                         landingPage
                         status
+                        dateAdded
+                        dateUpdated
                     } 
                 }
             `,
